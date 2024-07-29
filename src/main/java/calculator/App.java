fbad69bcba5b95ed37c3cc30d1b9da9aa8ec45bf
wjdtmdgjs <jeong5207@naver.com>
2024-07-29 11:14:48 +0900

    exit 입력하기 전까지 무한계산 진행

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,18 +5,43 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num2 = sc.nextInt();
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+            char operator = sc.next().charAt(0); // charAt(0) : 입력받는값을 char로 변환해준다.
+            //원래 scanner는 String으로 받아줘야된다.
+            // charAt('인덱스값')
+            int result = 0;
 
-        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        char operator = sc.next().charAt(0); // charAt(0) : 입력받는값을 char로 변환해준다.
-        //원래 scanner는 String으로 받아줘야된다.
-        // charAt('인덱스값')
+            if (operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌수 없습니다.");
+                } else {
+                    result = num1 / num2;
+                }
+            }
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
 
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+            sc.nextLine(); // 자꾸 넘어가서 개행문자를 받아줌
+            String text = sc.nextLine();
+
+            if (text.equals("exit")){ // exit 치면 빠져나가고 아님 계속 무한 계산하게
+                break;
+            }
+
+        }
     }
 }
